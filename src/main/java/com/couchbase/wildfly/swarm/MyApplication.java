a7fef3af905e92d2dea94ab9e9cf39e7bf3e6b0a
Arun Gupta <arun.gupta@gmail.com>
2016-03-18 14:11:27 -0700

    starting work on Couchbase + WildFly Swarm sample

---
+++ src/main/java/com/couchbase/wildfly/swarm/MyApplication.java
@@ -0,0 +1,12 @@
+package com.couchbase.wildfly.swarm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Arun Gupta
+ */
+@ApplicationPath("webresources")
+public class MyApplication extends Application {
+
+}
